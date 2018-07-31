84393fc0a8a588e57394f35b5a2228a3f48d9c66
abponder <32970225+abponder@users.noreply.github.com>
2018-07-31 10:27:35 -0700

    Delete reviewStatus.js

--- models/reviewStatus.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (sequelize, DataTypes)
-var reviewStatus = sequelize.define("reviewStatus", {
-    indexStatus: dataTypes.INTEGER,
-    region: dataTypes.STRING,
-    medicalCenter: dataTypes.STRING
-
-})
